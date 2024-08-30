6eba03703a509b41494bba14e74ad7a7aa01ef0d
KOTSIARYNAKA <katerinakarpuk91@gmail.com>
2024-08-30 23:18:35 +0300

    variable

--- src/HW1/surname.java
+++ src/HW1/surname.java
@@ -3,8 +3,16 @@ package HW1;
 public class surname {
     public static void main(String[] args) {
         System.out.println("Карпук");
-        int a = 3;
+        int a = -2147483648;
         System.out.println(a);
+
+        short b = 32767;
+        System.out.println(b);
+        byte c =-128;
+        System.out.println(c);
+        long d = -2147483648; // почему ограніченіе как на int?
+        System.out.println(d);
+
     }
 
 }
