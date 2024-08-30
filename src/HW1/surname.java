798e8814f1e2423d5525194ce6f8539534759909
KOTSIARYNAKA <katerinakarpuk91@gmail.com>
2024-08-30 22:42:17 +0300

    3

--- src/HW1/surname.java
+++ src/HW1/surname.java
@@ -3,5 +3,11 @@ package HW1;
 public class surname {
     public static void main(String[] args) {
         System.out.println("Карпук");
+        int a = 3;
+        System.out.println(a);
     }
+  //  int a;
+
+//    System.out.println("a");
+
 }
