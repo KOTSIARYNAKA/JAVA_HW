a36c0de5f5f9eb156a4c6f4c58f65c3a3dd71bfb
KOTSIARYNAKA <katerinakarpuk91@gmail.com>
2024-08-31 16:24:55 +0300

    scanner

---
+++ src/HW3/Main.java
@@ -0,0 +1,23 @@
+package HW3;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число a:");
+
+        int a = sc.nextInt();
+
+
+        System.out.println("Спасибо! Вы ввели число " + a);
+        System.out.println("Введите число b:");
+
+        int b = sc.nextInt();
+
+
+        System.out.println("Спасибо! Вы ввели число " + b);
+        System.out.println("Спасибо! Вы ввели число " + a);
+
+
+    }
+}
