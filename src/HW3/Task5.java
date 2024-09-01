99ce7b70d10fe574e54926dea73ff2266a7b17d3
KOTSIARYNAKA <katerinakarpuk91@gmail.com>
2024-09-01 19:57:54 +0300

    task5

--- src/HW3/Task5.java
+++ src/HW3/Task5.java
@@ -5,14 +5,11 @@ public class Task5 {
     int x = 5;
     int y = 2;
     int z = 6;
-    int c ;
     x--;
     y++;
-    z--;
-    c = x + y * z;
-    c++;
-    c++;
-    c++;
+    int c = x + y * z--;
+
         System.out.println("Результат: x = " + x + ", у = " + y + ", z = " + z + ", c = " + c);
+
     }
     }
