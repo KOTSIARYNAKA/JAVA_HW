a70521ffe77584a1af0258442fce70a8388c460c
KOTSIARYNAKA <katerinakarpuk91@gmail.com>
2024-09-02 20:21:36 +0300

    HW4task1

---
+++ src/HW4/Task1.java
@@ -0,0 +1,13 @@
+package HW4;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        int a = 100, b = 102, c = 5;
+                if (a % 2 == 0 ) System.out.println(a);
+                if (b % 2 == 0 ) System.out.println(b);
+                if (c % 2 == 0 ) System.out.println(c);
+
+
+    }
+}
