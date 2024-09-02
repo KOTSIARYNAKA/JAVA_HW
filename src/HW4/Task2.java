a5c81990d04bd56b7d6a61fe5d0fdd3b11320eaf
KOTSIARYNAKA <katerinakarpuk91@gmail.com>
2024-09-02 20:46:52 +0300

    HW4task2

---
+++ src/HW4/Task2.java
@@ -0,0 +1,10 @@
+package HW4;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int a = 11, b = 8, c = 5;
+        if (a % 2 == 0 || a % 5 == 0) System.out.println(a);
+        if (b % 2 == 0 || b % 5 == 0) System.out.println(b);
+        if (c % 2 == 0 || c % 5 == 0) System.out.println(c);
+    }
+}
