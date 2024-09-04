45403007aa2893275f7ba0e47a5577067177e982
KOTSIARYNAKA <katerinakarpuk91@gmail.com>
2024-09-04 21:12:48 +0300

    HW5

---
+++ src/HW5/Task1for.java
@@ -0,0 +1,9 @@
+package HW5;
+//Задание: Вывести числа от 1 до 100.
+public class Task1for {
+    public static void main(String[] args) {
+        for (int a = 1; a <= 100; a++) {
+            System.out.println(a + " ");
+        }
+    }
+}
---
+++ src/HW5/Task2for.java
@@ -0,0 +1,10 @@
+package HW5;
+
+public class Task2for {
+    public static void main(String[] args) {
+        for (int x = 1; x <=100 ; x++) {
+            if (x % 3 == 0 || x % 7 == 0)
+                System.out.println(x + " ");
+        }
+    }
+}
