00508d99d9ba1e8a8cedf379dd9742ad7c2c5c96
KOTSIARYNAKA <katerinakarpuk91@gmail.com>
2024-09-04 19:25:07 +0300

    HW4task3+while

--- src/HW4/Task3v2switch.java
+++ src/HW4/Task3v2switch.java
@@ -2,12 +2,16 @@ package HW4;
 
 import java.util.Scanner;
 
-public class Task3v2 {
+public class Task3v2switch {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите число x:");
         int x = sc.nextInt();
+        while (x==0){
+            System.out.println("На ноль делить нельзя, введите другое число: ");
+            x = sc.nextInt();}
+
         double result = (double) 15 / x;
         int i = 15 % x;
 
