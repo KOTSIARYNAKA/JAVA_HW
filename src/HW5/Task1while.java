f2c661f666a8baabffbb696ae62e3986d4c5aec6
KOTSIARYNAKA <katerinakarpuk91@gmail.com>
2024-09-04 19:48:19 +0300

    HW5task1while

--- src/HW5/Task1while.java
+++ src/HW5/Task1while.java
@@ -3,8 +3,8 @@ package HW5;
 public class Task1while {
     public static void main(String[] args) {
         int x = 1;
-        while (x<101) {
-            System.out.print(x + ", ");
+        while (x<=100) {
+            System.out.print(x + " ");
             x++;
         }
     }
