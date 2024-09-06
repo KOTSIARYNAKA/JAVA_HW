85641ea3684db0671a6137ede15e297c51c6b2de
KOTSIARYNAKA <katerinakarpuk91@gmail.com>
2024-09-06 20:27:39 +0300

    HWtask4arrays

--- src/HW6_arrays/Task4arrays.java
+++ src/HW6_arrays/Task4arrays.java
@@ -1,4 +1,24 @@
 package HW6_arrays;
 
 public class Task4arrays {
-}
+    public static void main(String[] args) {
+        int [] arr = new int[10];
+        for (int i = 0; i< arr.length;i++){
+            arr[i] = (int)(Math.random()*100);
+            System.out.println(arr[i]);
+
+        }
+
+        System.out.println();
+        for (int i = 0;i< arr.length;i++){
+            if (i%2 !=0) {
+                arr[i] = 0;
+            }
+
+         }
+        for (int i = 0; i< arr.length;i++) {
+            System.out.println(arr[i]);
+        }
+        }
+    }
+
