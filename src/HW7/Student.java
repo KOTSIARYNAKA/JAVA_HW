72153aa032869fa40a5bc753c57f7303288ef2ef
KOTSIARYNAKA <katerinakarpuk91@gmail.com>
2024-09-15 14:40:35 +0300

    HW7get_set

--- src/HW7/Student.java
+++ src/HW7/Student.java
@@ -8,13 +8,13 @@ package HW7;
 import java.util.Calendar;
 
 public class Student {          //Переменные
-    int id;
-    String surname;
-    String name;
-    String birth_date;
-    String faculty;
-    String course;
-    String group;
+    private int id;
+    private String surname;
+    private String name;
+    private String birth_date;
+    private String faculty;
+    private String course;
+    private String group;
 
                                  //Конструктор с параметрами
 
@@ -31,6 +31,58 @@ public class Student {          //Переменные
 
     public Student() {       //конструктор без параметров
     }
+
+    public int getId(){
+        return id;
+    }
+    public String getSurname() {
+        return surname;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getBirth_date() {
+        return birth_date;
+    }
+    public String getFaculty() {
+        return faculty;
+    }
+    public String getCourse(){
+        return course;
+    }
+    public String getGroup(){
+        return group;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public void setSurname(String surname){
+        this.surname = surname;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setBirth_date(String birth_date){
+        this.birth_date = birth_date;
+    }
+    public void setFaculty(String faculty){
+        this.faculty = faculty;
+    }
+    public void setCourse(String course){
+        this.course = course;
+    }
+    public void setGroup(String group){
+        this.group = group;
+    }
+
+
+
+
+
+
+
+
+
 
     public static void main(String[] args) {
         Student student1 = new Student(1052, "Max", "Bush", "12 september 2000", "Historical", "History of RB", "AA-001");
