85c05e3f6ee320612dea3c8b886768f9b21a81ec
KOTSIARYNAKA <katerinakarpuk91@gmail.com>
2024-09-18 19:58:01 +0300

    HW8_String

--- src/HW8_String/Task1.java
+++ src/HW8_String/Task1.java
@@ -3,14 +3,28 @@ package HW8_String;
 public class Task1 {
     public static void main(String[] args) {
 
-        teststring("I like Java!!!");
+        test("I like Java!!!");
 
     }
 
-    public static void teststring(String text){
+    public static void test(String text) {
         System.out.println(text);
-        System.out.println("Последний символ строки: " + text.charAt(text.length()-1));
+        System.out.println("Последний символ строки: " + text.charAt(text.length() - 1));
+        String s = text.endsWith("java!") ? "Строка заканчивается на \"java!\"" : "Строка не заканчивается на \"java!\"";
+        System.out.println(s);
+        if (text.startsWith("Добро пож")) {
+            System.out.println("Строка начинается на \"Добро пож\"");
+        } else {
+            System.out.println("Строка не начинается на \"Добро пож\"");
+        }
+        if (text.contains("Java")){
+            System.out.println("Строка содержит подстроку \"Java\"");
+        }
+        else {
+            System.out.println("Строка не содержит подстроку \"Java\"");
+        }
+      // text.indexOf(String "Java")
+
 
     }
-
 }
